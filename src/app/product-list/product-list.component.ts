0b7f07ed9d804a1a458b30574ddd76bac5a87501
viviangovender24 <102669928+viviangovender24@users.noreply.github.com>
2023-07-05 12:47:11 +0200

    3 files modified

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -10,9 +10,17 @@ import { products } from '../products';
 export class ProductListComponent {
   products = [...products];
 
-  share() {
+  share() 
+  {
     window.alert('The product has been shared!');
   }
+
+  onNotify() 
+  {
+    window.alert('You will be notified when the product goes on sale');
+  }
+
+
 }
 
 
